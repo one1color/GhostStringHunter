be4e830087ea598400c9557f5198465a1494c90f
one1color <koki.111.sui@gmail.com>
2023-04-15 01:56:00 +0200

    added new logic

--- Sources/GhostStringHunterBuildTool/main.swift
+++ Sources/GhostStringHunterBuildTool/main.swift
@@ -1,42 +1,40 @@
 import Foundation
 import GhostStringHunter
 
-func parseLocalizableStringsFile(atPath path: String) -> [(key: String, lineNumber: Int)] {
-    guard let fileContent = try? String(contentsOfFile: path) else { return [] }
+func findLocalizableStringsFiles(inDirectory directoryPath: String) -> [String] {
+    let fileManager = FileManager.default
+    guard let enumerator = fileManager.enumerator(atPath: directoryPath) else { return [] }
 
-    var results: [(key: String, lineNumber: Int)] = []
-    var currentLineNumber = 1
-
-    let lines = fileContent.components(separatedBy: .newlines)
-    for line in lines {
-        let trimmedLine = line.trimmingCharacters(in: .whitespaces)
-        if let range = trimmedLine.range(of: #"^"([\w_]+)"\s*=".*$"#, options: .regularExpression) {
-            let key = String(trimmedLine[range].dropFirst().dropLast().trimmingCharacters(in: .whitespaces))
-            results.append((key: key, lineNumber: currentLineNumber))
+    var localizableStringsFiles: [String] = []
+    while let filePath = enumerator.nextObject() as? String {
+        if filePath.hasSuffix("Localizable.strings") {
+            localizableStringsFiles.append(directoryPath + "/" + filePath)
         }
-        currentLineNumber += 1
     }
-
-    return results
+    return localizableStringsFiles
 }
 
 func main() {
-    // Locate the Localizable.strings file
-    let localizableStringsPath = "/path/to/LocalizeTest/Localizable.strings"
+    // Locate the current directory path
+    let currentDirectoryPath = FileManager.default.currentDirectoryPath
 
-    // Parse the Localizable.strings file to find the keys and their corresponding line numbers
-    let localizableKeysAndLineNumbers = parseLocalizableStringsFile(atPath: localizableStringsPath)
+    // Find all Localizable.strings files in the project
+    let localizableStringsFiles = findLocalizableStringsFiles(inDirectory: currentDirectoryPath)
 
-    // Add your logic to detect unused localized strings here
-    // For demonstration purposes, let's assume you detect that the first key is unused
-    if let unusedKey = localizableKeysAndLineNumbers.first {
+    // Process each Localizable.strings file
+    for localizableStringsPath in localizableStringsFiles {
+        // Add your logic to detect unused localized strings here
+        // For demonstration purposes, let's show a warning if a Localizable.strings file is found
         let warningFilePath = localizableStringsPath
-        let warningLineNumber = unusedKey.lineNumber
+        let warningLineNumber = 1 // Output the warning on the first line
         let warningColumnNumber = 1 // Column number is set to 1 since we don't have the exact column number
-        let warningMessage = "Unused localized string key '\(unusedKey.key)' detected"
+        let warningMessage = "Unused localized string detected"
 
-        // Output the warning message in Xcode's expected format
+        // Output the warning message in Xcode's expected format for the file
         print("\(warningFilePath):\(warningLineNumber):\(warningColumnNumber): warning: \(warningMessage)")
+
+        // Output a warning message for the navigation bar
+        print("warning: Localizable.strings file found at: \(localizableStringsPath)")
     }
 }
 
