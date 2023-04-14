280d63bd6fcc129e7e0b89e8d2ea9f18618f490d
one1color <koki.111.sui@gmail.com>
2023-04-14 17:35:18 +0200

    changed the way

--- Package.swift
+++ Package.swift
@@ -3,25 +3,24 @@ import PackageDescription
 
 let package = Package(
     name: "GhostStringHunter",
-    platforms: [
-        .macOS(.v11),
-    ],
     products: [
         .library(
             name: "GhostStringHunter",
             targets: ["GhostStringHunter"]),
+        .executable(
+            name: "GhostStringHunterExecutable",
+            targets: ["GhostStringHunterExecutable"])
     ],
     dependencies: [
+        // Add any dependencies here
     ],
     targets: [
-        .executableTarget(
-            name: "GhostStringHunterCLI",
-            dependencies: ["GhostStringHunter"]),
         .target(
             name: "GhostStringHunter",
             dependencies: []),
-        .testTarget(
-            name: "GhostStringHunterTests",
-            dependencies: ["GhostStringHunter"]),
+        .executableTarget(
+            name: "GhostStringHunterExecutable",
+            dependencies: ["GhostStringHunter"],
+            path: "Executable")
     ]
 )
