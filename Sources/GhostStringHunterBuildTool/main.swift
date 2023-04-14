dc9fb8909d6d67dc9683fa9ad4b89021c959a968
one1color <koki.111.sui@gmail.com>
2023-04-15 01:31:25 +0200

    warning in the code

--- Sources/GhostStringHunterBuildTool/main.swift
+++ Sources/GhostStringHunterBuildTool/main.swift
@@ -6,6 +6,10 @@ func main() {
     // You can use FileManager to scan the project directory for .strings and source files.
     // Then, parse the .strings files to get the keys and search for their usage in the source files.
     // Finally, print out the list of unused keys.
+    let warningFilePath = "localizeTest/Localizable.strings"
+    let warningLineNumber = 2
+    let warningColumnNumber = 5
+    let warningMessage = "Unused localized string detected"
     print("warning: GhostStringHunterBuildTool is running. (This is a test warning)")
 }
 
