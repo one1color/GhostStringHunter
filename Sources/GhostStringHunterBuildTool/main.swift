f26d8400af297e2da25109bb04d07563df4668df
one1color <koki.111.sui@gmail.com>
2023-04-15 01:38:16 +0200

    warning in the code

--- Sources/GhostStringHunterBuildTool/main.swift
+++ Sources/GhostStringHunterBuildTool/main.swift
@@ -1,16 +1,43 @@
 import Foundation
 import GhostStringHunter
 
+func parseLocalizableStringsFile(atPath path: String) -> [(key: String, lineNumber: Int)] {
+    guard let fileContent = try? String(contentsOfFile: path) else { return [] }
+
+    var results: [(key: String, lineNumber: Int)] = []
+    var currentLineNumber = 1
+
+    let lines = fileContent.components(separatedBy: .newlines)
+    for line in lines {
+        let trimmedLine = line.trimmingCharacters(in: .whitespaces)
+        if let range = trimmedLine.range(of: #"^"([\w_]+)"\s*=".*$"#, options: .regularExpression) {
+            let key = String(trimmedLine[range].dropFirst().dropLast().trimmingCharacters(in: .whitespaces))
+            results.append((key: key, lineNumber: currentLineNumber))
+        }
+        currentLineNumber += 1
+    }
+
+    return results
+}
+
 func main() {
-    // Your implementation for detecting unused localized strings goes here.
-    // You can use FileManager to scan the project directory for .strings and source files.
-    // Then, parse the .strings files to get the keys and search for their usage in the source files.
-    // Finally, print out the list of unused keys.
-    let warningFilePath = "localizeTest/Localizable.strings"
-    let warningLineNumber = 2
-    let warningColumnNumber = 5
-    let warningMessage = "Unused localized string detected"
-    print("warning: GhostStringHunterBuildTool is running. (This is a test warning)")
+    // Locate the Localizable.strings file
+    let localizableStringsPath = "/path/to/LocalizeTest/Localizable.strings"
+
+    // Parse the Localizable.strings file to find the keys and their corresponding line numbers
+    let localizableKeysAndLineNumbers = parseLocalizableStringsFile(atPath: localizableStringsPath)
+
+    // Add your logic to detect unused localized strings here
+    // For demonstration purposes, let's assume you detect that the first key is unused
+    if let unusedKey = localizableKeysAndLineNumbers.first {
+        let warningFilePath = localizableStringsPath
+        let warningLineNumber = unusedKey.lineNumber
+        let warningColumnNumber = 1 // Column number is set to 1 since we don't have the exact column number
+        let warningMessage = "Unused localized string key '\(unusedKey.key)' detected"
+
+        // Output the warning message in Xcode's expected format
+        print("\(warningFilePath):\(warningLineNumber):\(warningColumnNumber): warning: \(warningMessage)")
+    }
 }
 
 main()
