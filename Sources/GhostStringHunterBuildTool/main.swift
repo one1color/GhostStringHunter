bd66b3ffac96e14eb830a11670b7bb5faa5f0ded
one1color <koki.111.sui@gmail.com>
2023-04-15 02:07:31 +0200

    updated test

--- Sources/GhostStringHunterBuildTool/main.swift
+++ Sources/GhostStringHunterBuildTool/main.swift
@@ -1,10 +1,22 @@
 import Foundation
 import GhostStringHunter
 
+func findProjectPath(projectName: String, inDirectory directoryPath: String) -> String? {
+    let fileManager = FileManager.default
+    guard let enumerator = fileManager.enumerator(atPath: directoryPath) else { return nil }
+    
+    while let filePath = enumerator.nextObject() as? String {
+        if filePath.contains("\(projectName).xcodeproj") || filePath.contains("\(projectName).xcworkspace") {
+            return directoryPath + "/" + filePath
+        }
+    }
+    return nil
+}
+
 func findLocalizableStringsFiles(inDirectory directoryPath: String) -> [String] {
     let fileManager = FileManager.default
     guard let enumerator = fileManager.enumerator(atPath: directoryPath) else { return [] }
-
+    
     var localizableStringsFiles: [String] = []
     while let filePath = enumerator.nextObject() as? String {
         if filePath.hasSuffix("Localizable.strings") {
@@ -18,10 +30,20 @@ func main() {
     // Locate the current directory path
     let currentDirectoryPath = FileManager.default.currentDirectoryPath
     print("warning: search starting point(test): \(currentDirectoryPath)")
+    
+    let parentDirectoryPath = URL(fileURLWithPath: currentDirectoryPath).deletingLastPathComponent().path
+    
+    // Find the .xcodeproj or .xcworkspace file and the project root path for "LocalizeTest"
+    guard findProjectPath(projectName: "LocalizeTest", inDirectory: parentDirectoryPath) != nil else {
+        print("Error: Could not find the project root path.")
+        exit(1)
+    }
+    
+    
     // Find all Localizable.strings files in the project
     let localizableStringsFiles = findLocalizableStringsFiles(inDirectory: currentDirectoryPath)
     print("warning: found localizableString(test): \(localizableStringsFiles)")
-
+    
     // Process each Localizable.strings file
     for localizableStringsPath in localizableStringsFiles {
         // Add your logic to detect unused localized strings here
@@ -30,10 +52,10 @@ func main() {
         let warningLineNumber = 1 // Output the warning on the first line
         let warningColumnNumber = 1 // Column number is set to 1 since we don't have the exact column number
         let warningMessage = "Unused localized string detected"
-
+        
         // Output the warning message in Xcode's expected format for the file
         print("\(warningFilePath):\(warningLineNumber):\(warningColumnNumber): warning: \(warningMessage)")
-
+        
         // Output a warning message for the navigation bar
         print("warning: Localizable.strings file found at: \(localizableStringsPath)")
     }
